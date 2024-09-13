ab8641eaf3147baa22a9284df1b84c92542467ac
sungu <sungu@ASUSALI>
2024-09-13 23:08:39 +0300

    Add project files.

---
+++ otomasyon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string folderPath = @"C:\Users\sungu\OneDrive\Masaüstü\Fatura\"; // Dosya yolunu aldık
+
+        string ftrFileName = "I-FTR"+DateTime.Now.ToShortDateString()+"-00001.txt";
+        string fdyFileName = "I-FDY20240927-00001.txt";
+
+        // Klasördeki tüm dosyaların listesini al
+        string[] files = Directory.GetFiles(folderPath);
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            // Dosya yolunu al
+            string filePath = files[i];
+
+            // Dosya adını al
+            string fileName = Path.GetFileName(filePath);
+
+            if (File.Exists(filePath))
+            {
+                string dateSuffix = DateTime.Now.ToString("yyyyMMddHHmm");
+                string newFileName = "";
+
+                if (fileName == ftrFileName)
+                {
+                    // Yeni dosya adını oluşturun
+                    newFileName = "ftrFile-" + dateSuffix + Path.GetExtension(filePath);
+                    string newFilePath = Path.Combine(folderPath, newFileName);
+                    File.Move(filePath, newFilePath);
+                    Debug.WriteLine($"Dosya yeniden adlandırıldı: {filePath} -> {newFilePath}");
+                }
+                else if (fileName == fdyFileName)
+                {
+                    newFileName = "fdyFile-" + dateSuffix + Path.GetExtension(filePath);
+                    string newFilePath = Path.Combine(folderPath, newFileName);
+                    File.Move(filePath, newFilePath);
+                    Debug.WriteLine($"Dosya yeniden adlandırıldı: {filePath} -> {newFilePath}");
+                }
+            }
+            else
+            {
+                Debug.WriteLine($"Dosya bulunamadı: {filePath}");
+            }
+        }
+    }
+}
